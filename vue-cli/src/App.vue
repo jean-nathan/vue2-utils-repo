d95d4dce43c97a8ed5676a381a83070a6cae51b6
jean-nathan <nathan.ecom@outlook.com>
2025-03-03 18:28:43 -0300

    add async and  gitignore

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,30 +1,29 @@
 <template>
   <div id="app">
-    <button @click="componentActive = 'VueCliHeader'">Renderizar componente VueCliHeader</button>
-    <button @click="componentActive = 'VueCliFooter'">Renderizar componente VueCliFooter</button>
+    <button @click="componentActive = 'VueCliHeader'">VueCliHeader</button>
+    <button @click="componentActive = 'VueCliFooter'">VueCliFooter</button>
     <keep-alive>
-      <component :is="componentActive"></component>
+    <component :is="componentActive"></component>
     </keep-alive>
   </div>
 </template>
 
 <script>
-import VueCliHeader from './components/VueCliHeader.vue';
-import VueCliFooter from './components/VueCliFooter.vue';
-
 export default {
   name: 'App',
   data() {
     return {
-      componentActive: "VueCliHeader"
+      componentActive: ''  // Atribuir o nome do componente como string
     }
   },
   components: {
-    VueCliHeader,
-    VueCliFooter
+    VueCliHeader: () => import("./components/VueCliHeader.vue"),
+    VueCliFooter: () => import("./components/VueCliFooter.vue")
   }
 }
 </script>
+
+
 
 <style>
 #app {
