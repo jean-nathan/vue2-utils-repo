cff8ca0258b554f1781dcbb48ed8c9aacaa866b0
jean-nathan <nathan.ecom@outlook.com>
2025-03-04 20:37:58 -0300

    add file transition

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,37 +1,78 @@
 <template>
   <div id="app">
-    <button @click="componentActive = 'VueCliHeader'">VueCliHeader</button>
-    <button @click="componentActive = 'VueCliFooter'">VueCliFooter</button>
-    <keep-alive>
-    <component :is="componentActive"></component>
-    </keep-alive>
+    <div class="modal" v-if="active">
+      <div class="modal-container">
+        <h1>Modal Aberto</h1>
+        <p>Seu modal foi aberto com sucesso.</p>
+        <button @click="active = !active">fechar</button>
+      </div>
+    </div>
+    <button @click="active = !active">Abrir modal</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      componentActive: ''  // Atribuir o nome do componente como string
-    }
+      active: false,
+    };
   },
-  components: {
-    VueCliHeader: () => import("./components/VueCliHeader.vue"),
-    VueCliFooter: () => import("./components/VueCliFooter.vue")
+  components: {},
+  methods: {
+    enter(element) {
+      console.log(element.classList.add("teste"));
+    },
+    leave(element) {
+      console.log("Elemento sumiu!",element)
+    }
   }
-}
+};
 </script>
 
 
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
+
+  .modal::before {
+    content: "";
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(166, 165, 165, 0.5);
+  }
+
+  .modal {
+  display: flex;
+  flex-direction: column;
+  justify-content: center; /* Centraliza horizontalmente */
+  align-items: center; /* Centraliza verticalmente */
+  height: 100vh;
+  }
+
+  .modal-container {
+    background: white;
+    z-index: 1;
+    padding: 70px;
+    text-align: center;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.4s;
+  }
+  
+  .fade-enter,
+  .fade-leave-to
+  {
+    opacity: 0;
+  }
+
 </style>
