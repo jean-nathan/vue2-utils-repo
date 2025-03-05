7dafcd74bbbbc35faa8348b1d8bf75e91f5271fa
jean-nathan <nathan.ecom@outlook.com>
2025-03-04 22:55:49 -0300

    done exercise

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <div class="modal" v-if="active">
-      <div class="modal-container">
-        <h1>Modal Aberto</h1>
-        <p>Seu modal foi aberto com sucesso.</p>
-        <button @click="active = !active">fechar</button>
-      </div>
-    </div>
-    <button @click="active = !active">Abrir modal</button>
+    <button @click="clicked">Abrir Modal</button>
+    <transition name="modal">
+      <VueCliModal 
+      v-if="active"
+      :hasActive="clicked"
+      />
+    </transition>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "App",
   data() {
@@ -19,60 +19,30 @@ export default {
       active: false,
     };
   },
-  components: {},
+  components: {
+    VueCliModal: () => import("./components/VueCliModal.vue")
+  },
   methods: {
-    enter(element) {
-      console.log(element.classList.add("teste"));
-    },
-    leave(element) {
-      console.log("Elemento sumiu!",element)
+    clicked() {
+      this.active = !this.active
     }
   }
-};
+}
 </script>
 
 
-<style>
-  body {
-    margin: 0;
-  }
+<style scoped>
+* {
+  box-sizing: border-box;
+}
 
-  .modal::before {
-    content: "";
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background: rgba(166, 165, 165, 0.5);
-  }
+.modal-enter,
+.modal-leave-to {
+  opacity: 0;
+}
 
-  .modal {
-  display: flex;
-  flex-direction: column;
-  justify-content: center; /* Centraliza horizontalmente */
-  align-items: center; /* Centraliza verticalmente */
-  height: 100vh;
-  }
-
-  .modal-container {
-    background: white;
-    z-index: 1;
-    padding: 70px;
-    text-align: center;
-    border-radius: 10px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  }
-
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.4s;
-  }
-  
-  .fade-enter,
-  .fade-leave-to
-  {
-    opacity: 0;
-  }
-
+.modal-enter-active,
+.modal-leave-to {
+  transition: opacity .3s;
+}
 </style>
